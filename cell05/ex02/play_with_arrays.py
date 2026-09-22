f33c31770c8e2d010303b4850d461257566a121f
mpitaksa <mekasith32@gmail.com>
2026-09-22 21:00:25 +0700

    update play_with_arrays.py

--- cell05/ex02/play_with_arrays.py
+++ cell05/ex02/play_with_arrays.py
@@ -2,7 +2,7 @@ original_array = [2, 8, 9, 48, 8, 22, -12, 2]
 new_array = []
 
 for num in original_array:
-    if (num >= 5):
+    if num > 5:
         new_array.append(num + 2)
 
 print("Original array:", original_array)
