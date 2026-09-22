4926050f113fc715b7c252f442c9182b5a35f07e
mpitaksa <mekasith32@gmail.com>
2026-09-22 19:02:01 +0700

    add calculator.py

---
+++ cell04/ex02/calculator.py
@@ -0,0 +1,8 @@
+num1 = int(input("Give me the first number: "))
+num2 = int(input("Give me the second number: "))
+
+print("Thank you!")
+print(str(num1) + " + " + str(num2) + " = " + str(num1 + num2))
+print(str(num1) + " - " + str(num2) + " = " + str(num1 - num2))
+print(str(num1) + " / " + str(num2) + " = " + str(num1 // num2))
+print(str(num1) + " * " + str(num2) + " = " + str(num1 * num2))
