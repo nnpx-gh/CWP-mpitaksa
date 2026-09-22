e18f796d5e1d07b3a8fd8589c3c74adb707f51f5
mpitaksa <mekasith32@gmail.com>
2026-09-22 20:09:35 +0700

    add play_with_arrays.py

---
+++ cell05/ex01/play_with_arrays.py
@@ -0,0 +1,8 @@
+original_array = [2, 8, 9, 48, 8, 22, -12, 2]
+new_array = []
+
+for num in original_array:
+    new_array.append(num + 2)
+
+print("Original array:", original_array)
+print("New array: ", new_array)
